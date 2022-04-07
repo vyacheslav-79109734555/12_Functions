0b1f3220a8dead997e2c028dca4348f39888dc5b
Вячеслав <95027060+vyacheslav-79109734555@users.noreply.github.com>
2022-04-07 10:02:36 +0300

    Апгрейд калькулятора

---
+++ _Домашняя работа/4_Число наоборот/Число наоборот_1.py
@@ -0,0 +1,31 @@
+def number():
+    numb = input('\nВведите последовательность чисел: ')
+    quantity = len(numb)
+    if int(numb) == 0 and numb.isdigit():
+        stop()
+    elif numb.isdigit() > 0:
+        a = int(quantity)
+        numb = int(numb)
+        backwards_numb(numb, a)
+    else:
+        print('Ошибка ввода! попробуйте еще раз: ')
+        number()
+
+
+def backwards_numb(numb, a):
+    print('Число наоборот: ', end='')
+    n = 0
+    for i in range(1, a + 1):
+        if i != 0:
+            result = numb % 10
+            a = numb // 10
+            numb = a
+        print(result, end=' ')  # цифры на разных строках
+    number()
+
+
+def stop():
+    print('Программа завершена!')
+
+
+number()
---
+++ _Домашняя работа/4_Число наоборот/Число наоборот_2.py
@@ -0,0 +1,30 @@
+def number():
+    numb = input('Введите последовательность чисел: ')
+    quantity = len(numb)
+    if int(numb) == 0 and numb.isdigit():
+        stop()
+    elif numb.isdigit() > 0:
+        a = int(quantity)
+        numb = int(numb)
+        backwards_numb(numb, a)
+    else:
+        print('Ошибка ввода! попробуйте еще раз: ')
+        number()
+
+
+def backwards_numb(numb, a):
+    n = 0
+    for i in range(1, a + 1):
+        result = numb % 10
+        numb = numb // 10
+        n = n * 10
+        n = n + result
+    print(f'Число наоборот: {n} ')
+    number()
+
+
+def stop():
+    print('Программа завершена!')
+
+
+number()
